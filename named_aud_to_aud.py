9d8cb39fea9f58cd0d3389a2f6cfc738d35a7e31
Jabulani-N <jabulani.ndh@gmail.com>
2025-04-26 15:45:40 -0500

    added test audio file and created default values
    basic functionality is NOT READY TO TEST yet. Still researching how to use pydub

--- named_aud_to_aud.py
+++ named_aud_to_aud.py
@@ -15,3 +15,18 @@ default directories exist, but should be specified when used.
 """
 
 from pydub import AudioSegment
+
+
+def named_aud_to_aud(input_aud_name="Floor of History.wav",
+                     output_aud_name="Floor of History.mp3",
+                     source_loc='./input/',
+                     save_loc='./output/'):
+
+    print("converter args recieved:")
+    print("input vid name:", input_aud_name)
+    print("source loc:", source_loc)
+    print("output aud name:", output_aud_name)
+    print("save loc:", save_loc)
+
+    aud_in = source_loc + input_aud_name
+    aud_out = save_loc + output_aud_name
