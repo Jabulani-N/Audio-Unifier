2a161fed26acb91844611b0c7fb1877722e75e63
Jabulani-N <jabulani.ndh@gmail.com>
2025-06-20 03:51:49 -0500

    script name update

--- ffmpeg_to_pcm_s16le_codec.py
+++ ffmpeg_to_pcm_s16le_codec.py
@@ -22,7 +22,7 @@ import os
 import sys
 
 
-def named_aud_to_aac(input_vid_name, output_aud_name,
+def named_aud_to_pcm16(input_vid_name, output_aud_name,
                      source_loc='./input/',
                      save_loc='./output/'):
     """
@@ -81,5 +81,5 @@ and automatically.\
     aud_file_to_give = input("What will you name the converted file?: ")
     print("you want to take",aud_file_to_take,
           "and give audio file", aud_file_to_give)
-    named_aud_to_aac(aud_file_to_take,
+    named_aud_to_pcm16(aud_file_to_take,
                      aud_file_to_give)
