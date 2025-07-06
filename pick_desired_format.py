f5fba64dec9fb875454268d27d21d51e67335f2d
Jabulani-N <jabulani.ndh@gmail.com>
2025-07-06 07:28:37 -0500

    format selector using a grid. don't use this
    grid prevents columns from overlapping, so if one is a long line of text, you can't have two adjascent buttons both under that line of text

--- pick_desired_format.py
+++ pick_desired_format.py
@@ -27,30 +27,22 @@ def ask_two_options(box_title="Format selection",
     dialog.geometry("600x250")
     dialog.resizable(False, False)
 
-    dialog.rowconfigure(0, weight=0)
-    dialog.rowconfigure(1, weight=0)
-    dialog.rowconfigure(2, weight=0)
-    dialog.rowconfigure(3, weight=0)
-    dialog.columnconfigure(0, weight=0)
-    # weight on a column makes it consume free space
-    dialog.columnconfigure(0, weight=1)
-    dialog.columnconfigure(2, weight=1)
     # Variable to store the user's choice
     user_choice = tk.StringVar(value="Cancel")  # Default to "Cancel"
 
     # Label with the message
-    tk.Label(dialog, text=box_text).grid(column=1, pady=25)
+    tk.Label(dialog, text=box_text).pack(column=1, pady=25)
 
     # Buttons for the options
     def select_option(option):
         user_choice.set(option)
         dialog.destroy()  # Close the dialog
 
-    tk.Button(dialog, text=opt1, command=lambda: select_option(opt1)).grid(column=0, row=2, pady=5, padx=1)
-    tk.Button(dialog, text=opt2, command=lambda: select_option(opt2)).grid(column=0, row=3, pady=1, padx=1)
-    tk.Button(dialog, text=opt3, command=lambda: select_option(opt3)).grid(column=2, row=2, pady=5, padx=1)
-    tk.Button(dialog, text=opt4, command=lambda: select_option(opt4)).grid(column=2, row=3, pady=1, padx=1)
-    tk.Button(dialog, text="Cancel", command=lambda: select_option("cancel")).grid(padx=1, pady=1, row=4, column=1)
+    tk.Button(dialog, text=opt1, command=lambda: select_option(opt1)).pack(side="center")
+    tk.Button(dialog, text=opt2, command=lambda: select_option(opt2)).pack(side="center")
+    tk.Button(dialog, text=opt3, command=lambda: select_option(opt3)).grid(side="center")
+    tk.Button(dialog, text=opt4, command=lambda: select_option(opt4)).pack(side="center")
+    tk.Button(dialog, text="Cancel", command=lambda: select_option("cancel")).pack(padx=1, pady=1, row=4, column=1)
 
     # Wait for the user to make a selection
     dialog.wait_window()
