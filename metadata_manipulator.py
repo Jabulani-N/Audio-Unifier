5ffd84edd30b37dcdbbb0cf65d4fb7d9a9c61dd8
Jabulani-N <jabulani.ndh@gmail.com>
2026-05-16 04:42:19 -0500

    began work on metadata
    updated and creted some relevant files, including metadata file that will have scripts for reading and writing metadata, to be summoned by the file that loops through creating the new audio files

---
+++ metadata_manipulator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+contains modules for
+    acquiring metadata
+    assigning metadata
+of an audio file
+
+different formats have different modules
+"""
+
+import mutagen
