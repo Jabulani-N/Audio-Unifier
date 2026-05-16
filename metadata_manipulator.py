891eb599a08291038b95dbbbe92e81f7762272ea
Jabulani-N <jabulani.ndh@gmail.com>
2026-05-16 05:15:19 -0500

    created a successful scan of all metadata
    it's not usable as a print, but it should be good if we just run the entire loop on each file, and just copy all previous metadata to insert into the created file

--- metadata_manipulator.py
+++ metadata_manipulator.py
@@ -9,3 +9,21 @@ different formats have different modules
 """
 
 import mutagen
+
+def retrieve_metadata_mp3(source_address):
+    source_audio=mutagen.File(source_address)
+
+    # go through all present metadata
+    for key, value in source_audio.items():
+        # debug print
+        print("time to find some metadata!")
+        print(f"{key}: {value}")
+
+
+if __name__ == '__main__':
+    # test mode
+    print("you are running a test for metadata_manipulator!\
+\nyou'll be prompted to enter a mp3 file to list metadata\
+\n\nGood luck!")
+    source_audio = input("what file do you wanna investigate?")
+    retrieve_metadata_mp3(source_audio)
